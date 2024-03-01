d7b86b55c6cd9dbfd30e82b0f8aac85ce16cec94
Call me Zuy <89380265+DuyTa506@users.noreply.github.com>
2024-03-01 16:50:58 +0700

    Update api.py

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from context_gen import load_context, load_embed_model
 from constant import llm_model_name, llm_token, embedding_path, corpus_embedding, data_path
 import torch
-from main import generate_answer
+from main import generate_answer, generate_answer_with_api
 app = FastAPI()
 
 def load_llm(llm_model_name, llm_token):
@@ -34,7 +34,7 @@ def generate_answer_endpoint(question_data: Question):
 
         question = question_data.question
 
-        response = generate_answer(question=question, llm_model=llm_model, tokenizer=token,
+        response = generate_answer_with_api(question=question, llm_model=llm_model, tokenizer=token,
                                    bm25=bm25, embedding_corpus=embedding_corpus, meta_corpus=meta_corpus,
                                    embedding_model=embedding_model)
 
